32df786d494a92207c07a69a697a2cf691c37f7c
xlzhengxxx <xiaolong_995@126.com>
2020-05-21 16:59:54 +0800

    后台代码

---
+++ src/test/java/com/zxl/card/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
